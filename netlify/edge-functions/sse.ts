6aee38ad182e5891324c1e7d2a9be60f566113c3
Matt Kane <m@mk.gg>
2022-05-09 12:19:52 +0100

    Initial commit

---
+++ netlify/edge-functions/sse.ts
@@ -0,0 +1,23 @@
+export default async function sse(request: Request) {
+  let timerId: number | undefined;
+  const body = new ReadableStream({
+    start(controller) {
+      timerId = setInterval(() => {
+        const msg = new TextEncoder().encode(
+          `data: hello at ${new Date().toUTCString()}\r\n\r\n`,
+        );
+        controller.enqueue(msg);
+      }, 1000);
+    },
+    cancel() {
+      if (typeof timerId === "number") {
+        clearInterval(timerId);
+      }
+    },
+  });
+  return new Response(body, {
+    headers: {
+      "Content-Type": "text/event-stream",
+    },
+  });
+}
